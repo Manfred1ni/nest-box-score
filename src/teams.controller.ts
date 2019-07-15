842ca1291deeac8727c82be83e12799437cee688
Manfred1ni <cheblimanfred@gmail.com>
2019-07-15 12:02:22 +0200

    team service

--- src/teams.controller.ts
+++ src/teams.controller.ts
@@ -1,47 +1,36 @@
-import { Controller, Get, Param } from '@nestjs/common';
+import { Controller, Get, Param } from "@nestjs/common";
+import { TeamsService } from "./teams.service";
+import { Team } from "./interfaces/team.interface";
 
-@Controller('teams')
+@Controller("teams")
 export class TeamsController {
+  constructor(private readonly teamsService: TeamsService) {}
+
   @Get()
-  findAll(): Array<Object> {
-    return [
-      {
-        id: 1,
-        name: 'Brewers',
-        shortName: 'MIL',
-        wonGames: '42',
-        lostGames: '38',
-      },
-      {
-        id: 2,
-        name: 'Mariners',
-        shortName: 'SEA',
-        wonGames: '37',
-        lostGames: '47',
-      },
-    ];
+  findAll(): Promise<Team[]> {
+    return this.teamsService.findAll();
   }
 
-  @Get(':id')
-  findOne(@Param('id') id: string) {
+  @Get(":id")
+  findOne(@Param("id") id: string) {
     let team = {};
     switch (id) {
-      case '1':
+      case "1":
         team = {
           id: 1,
-          name: 'Brewers',
-          shortName: 'MIL',
-          wonGames: '42',
-          lostGames: '38',
+          name: "Brewers",
+          shortName: "MIL",
+          wonGames: "42",
+          lostGames: "38"
         };
         break;
-      case '2':
+      case "2":
         team = {
           id: 2,
-          name: 'Mariners',
-          shortName: 'SEA',
-          wonGames: '37',
-          lostGames: '47',
+          name: "Mariners",
+          shortName: "SEA",
+          wonGames: "37",
+          lostGames: "47"
         };
         break;
     }
---
+++ src/teams.service.ts
@@ -0,0 +1,30 @@
+import { Injectable } from "@nestjs/common";
+import { Team } from "./interfaces/team.interface";
+
+@Injectable
+export class TeamsService {
+  private readonly teams: Team[] = [
+    {
+      id: 1,
+      name: "Brewers",
+      shortName: "MIL",
+      wonGames: 42,
+      lostGames: 38
+    },
+    {
+      id: 2,
+      name: "Mariners",
+      shortName: "SEA",
+      wonGames: 37,
+      lostGames: 47
+    }
+  ];
+
+  create(teams: Team) {
+    this.teams.push(teams);
+  }
+
+  findAll(): Team[] {
+    return this.teams;
+  }
+}
